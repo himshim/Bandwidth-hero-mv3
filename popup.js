9b7700f924d10fbfdf45f0e41d26c239a500b503
himshim26 <97538806+himshim@users.noreply.github.com>
2025-08-24 15:35:31 +0530

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,3 +1,9 @@
 const enabled = document.getElementById('enabled');
-chrome.storage.sync.get({ enabled: true }, d => { enabled.checked = d.enabled; });
-enabled.addEventListener('change', () => chrome.storage.sync.set({ enabled: enabled.checked }));
+
+chrome.storage.sync.get({ enabled: true }, (d) => {
+  enabled.checked = !!d.enabled;
+});
+
+enabled.addEventListener('change', () => {
+  chrome.storage.sync.set({ enabled: enabled.checked });
+});
